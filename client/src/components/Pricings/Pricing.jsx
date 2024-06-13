86b7e1d1e9214ab4e20387dbd0d0c35426a36ec8
KOUAKOU Georgio <georgino.kouak@gmail.com>
2024-06-13 18:13:11 +0000

    Add new file that assemble LowPricing and HighPricing components

--- client/src/components/Pricings/Pricing.jsx
+++ client/src/components/Pricings/Pricing.jsx
@@ -1,10 +1,11 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { SvgIconTransfer, SvgIconsValidate } from "../SvgIcons/SvgIcons"
 import ImgRefused from '../../assets/img/Refused.png'
 
 export const LowPricing = () => {
+    const navigate = useNavigate();
     return (
-        <Link className="py-12 px-4 flex flex-col rounded-xl border-2 border-dashed border-gray-400 text-center transition duration-75 hover:rotate-1 hover:scale-105 focus:rotate-1 focus:scale-105">
+        <Link to={`/PaymentFormulas/Basic_Souscription`} className="py-12 px-4 flex flex-col rounded-xl border-2 border-dashed border-gray-400 text-center transition duration-75 hover:rotate-1 hover:scale-105 focus:rotate-1 focus:scale-105">
             <div>
                 <h3 className="px-6 py-2 inline-block text-2xl md:text-3xl font-bold bg-teal-500 text-white -rotate-1">
                     Souscription Basique</h3>
@@ -54,7 +55,7 @@ export const LowPricing = () => {
             </ul>
 
             <div className="mt-16">
-                <div className="group inline-flex items-center justify-center text-md md:text-xl font-bold text-white px-6 py-3 rounded bg-teal-500 hover:bg-sky-600 focus:bg-blue-600">
+                <div onClick={() => navigate(`/PaymentFormulas/Basic_Souscription`)} className="group cursor-pointer inline-flex items-center justify-center text-md md:text-xl font-bold text-white px-6 py-3 rounded bg-teal-500 hover:bg-sky-600 focus:bg-blue-600">
                     Cliquer pour choisir ce forfait
                     <SvgIconTransfer />
                 </div>
@@ -64,8 +65,9 @@ export const LowPricing = () => {
 }
 
 export const HighPricing = () => {
+    const navigate = useNavigate(); 
     return (
-        <Link className="pricing-custom py-12 px-4 flex flex-col rounded-xl bg-white text-gray-900 text-center transition duration-75 hover:-rotate-1 hover:scale-105 focus:-rotate-1 focus:scale-105">
+        <Link to={`/PaymentFormulas/VIP_Souscription`} className="pricing-custom py-12 px-4 flex flex-col rounded-xl bg-white text-gray-900 text-center transition duration-75 hover:-rotate-1 hover:scale-105 focus:-rotate-1 focus:scale-105">
             <div>
                 <h3 className="px-6 py-2 inline-block text-2xl md:text-3xl font-bold bg-gray-900 text-white -rotate-1">
                     Souscription VIP</h3>
@@ -101,8 +103,7 @@ export const HighPricing = () => {
             </ul>
 
             <div className="mt-16">
-                <div
-                    className="group inline-flex items-center justify-center text-md md:text-xl font-bold text-white px-6 py-3 rounded  bg-teal-500 hover:bg-sky-600 focus:bg-blue-600">
+                <div onClick={() => navigate(`/PaymentFormulas/VIP_Souscription`)} className="group cursor-pointer inline-flex items-center justify-center text-md md:text-xl font-bold text-white px-6 py-3 rounded bg-teal-500 hover:bg-sky-600 focus:bg-blue-600">
                     Cliquer pour choisir ce forfait
                     <SvgIconTransfer />
                 </div>
