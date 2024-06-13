c42a874f7d22dfe123669c02c44c94974261cb18
KOUAKOU Georgio <georgino.kouak@gmail.com>
2024-06-13 17:54:50 +0000

    Update text of Scrolling Text

--- client/src/components/ScrollingText/ScrollingText.jsx
+++ client/src/components/ScrollingText/ScrollingText.jsx
@@ -4,8 +4,7 @@ const ScrollingText = () => {
     return (
         <div className="relative bg-blue-200 h-10 overflow-scroll w-auto snap-x scrollingText-custom">
             <div className="absolute h-full w-max flex items-center p-2 scrollingText-custom-containText">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos, sapiente. Suscipit explicabo, quasi architecto rem magni mollitia voluptatibus minima maiores, dolores enim ipsam, beatae consectetur consequuntur minus odit deleniti corrupti!
-                || Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex dolorem quam ad veritatis iste voluptate ducimus soluta enim, dignissimos reiciendis repellendus nostrum voluptatibus ullam dolorum, facilis consequatur, quia provident. Expedita!
+                Société de gestion locative, courtier immobilier, recouvrez désormais les loyers, les frais de dossiers ou les cautions via votre plateforme de paiement digital AIGLE !!
             </div>
         </div>
     )
