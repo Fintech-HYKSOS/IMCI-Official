6f76a01d20a797c8a87ea4634730486d0d1029cc
KOUAKOU Georgio <georgino.kouak@gmail.com>
2024-06-18 19:05:59 +0000

    Update HeroAllSociety by styling z-index of Map

--- client/src/components/Heros/HeroAllSociety/HeroAllsociety.jsx
+++ client/src/components/Heros/HeroAllSociety/HeroAllsociety.jsx
@@ -3,11 +3,6 @@ import Image from '../../../assets/img/houses/banier-house-withoutbg.png'
 import { housesData } from "../../../data"
 import Maping from "../../MapForHeroAllSociety/Maping"
 
-// import Folourgo_Logo from '../../../assets/folourgo/Folourgo_Logo.jpg'
-// import SCI_Kerlau_Logo from '../../../assets/Kerlau/KERLAU_Logo.jpeg'
-// const { logoSociety } = housesData
-// const DisplayLogo = null
-
 /**
  * A JSX Components for render the good title of the content page (actually based on static URL manipulation)
  * @returns {string} displayTitle - Title of the Content
@@ -25,16 +20,6 @@ const RenderTitleSociety = () => {
 
     return displayTitle
 }
-
-// const RenderLogoSociety = () => {
-//     let displayLogo
-
-//     location.pathname == '/SocietyProperties/Folourgo'? displayLogo=Folourgo_Logo
-//     : location.pathname == '/SocietyProperties/Agence_Succes'? displayLogo=Folourgo_Logo
-//     : location.pathname == '/SocietyProperties/SCI_Kerlau'? displayLogo=Folourgo_Logo
-//     : location.pathname == '/SocietyProperties/Cinabre'? displayLogo=Folourgo_Logo
-//     : displayLogo=null
-// }
 
 
 /**
@@ -60,8 +45,6 @@ const RenderLogoSociety = () => {
             : RenderTitleSociety() === "SCI KERLAU" ? displayLogo = housesData.find(house => house.society === "SCI KERLAU")
                 : RenderTitleSociety() === "CINABRE IMMOBILIER" ? displayLogo = housesData.find(house => house.society === "CINABRE IMMOBILIER")
                     : displayLogo = null
-
-    // console.log('displayLogo', displayLogo)
     return displayLogo.logoSociety
 }
 
@@ -83,7 +66,7 @@ const BannerAllSociety = () => {
                     {/* <div className="z-0 w-full h-52 sm:w-96 sm:h-52 mb-4">
                         <Maping/>
                     </div> */}
-                    <div className="w-full h-56 sm:w-96 sm:h-56 mb-4">
+                    <div className="w-full relative z-10 h-56 sm:w-96 sm:h-56 mb-4">
                         <Maping />
                     </div>
                 </div>
@@ -94,21 +77,7 @@ const BannerAllSociety = () => {
                     <img className='h-full w-full object-contain' src={RenderLogoSociety()} alt='' />
                 </div>
             </div>
-            {/* <ButtonForSearchKheopx /> */}
-
         </section>
-        // <div className={`h-64 rounded-md overflow-hidden bg-cover bg-center mt-3`} style={{ backgroundColor: `black` }}>
-        //     <div class="bg-gray-900 bg-opacity-50 flex items-center h-full">
-        //         <div class="px-10">
-        //             <h2 class="text-2xl text-white font-semibold text-center">Sport Shoes</h2>
-        //             <p class="mt-2 text-gray-400">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore facere provident molestias ipsam sint voluptatum pariatur.</p>
-        //             <button class="flex items-center mt-4 px-3 py-2 bg-blue-600 text-white text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
-        //                 <span>Shop Now</span>
-        //                 <svg class="h-5 w-5 mx-2" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M17 8l4 4m0 0l-4 4m4-4H3"></path></svg>
-        //             </button>
-        //         </div>
-        //     </div>
-        // </div>
     )
 }
 
